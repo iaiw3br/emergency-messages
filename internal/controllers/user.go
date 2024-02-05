596bde00017b2e84642d05af1c85409c8f0744a0
Artem Ivanov <artem.ivanov@onetwotrip.com>
2024-02-05 22:05:06 +0300

    use controllers instead of using handlers

--- internal/controllers/user.go
+++ internal/controllers/user.go
@@ -1,42 +1,31 @@
-package handlers
+package controllers
 
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"net/http"
 	"projects/emergency-messages/internal/logging"
 	"projects/emergency-messages/internal/services"
-
-	"github.com/go-chi/chi/v5"
 )
 
-const users = "/users"
-
-type UserController struct {
+type User struct {
 	userService services.UserService
 	log         logging.Logger
 }
 
-func (u UserController) Register(r *chi.Mux) {
-	r.Get(fmt.Sprintf("%s/city/:city", users), u.GetByCity)
-	r.Post(fmt.Sprintf("%s/upload", users), u.Upload)
-}
-
-func NewUser(userService services.UserService, log logging.Logger) UserController {
-	return UserController{
+func NewUser(userService services.UserService, log logging.Logger) *User {
+	return &User{
 		userService: userService,
 		log:         log,
 	}
 }
 
-func (u UserController) GetByCity(w http.ResponseWriter, r *http.Request) {
+func (u User) GetByCity(w http.ResponseWriter, r *http.Request) {
 	city := r.URL.Query().Get("city")
 	ctx := context.Background()
 	users, err := u.userService.GetByCity(ctx, city)
-	if httpCode := assertError(err); httpCode != 0 {
+	if assertError(err, w) {
 		u.log.Error("User.GetByCity() error:", err)
-		http.Error(w, http.StatusText(httpCode), httpCode)
 		return
 	}
 
@@ -51,7 +40,7 @@ func (u UserController) GetByCity(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 }
 
-func (u UserController) Upload(w http.ResponseWriter, r *http.Request) {
+func (u User) Upload(w http.ResponseWriter, r *http.Request) {
 	file, _, err := r.FormFile("file")
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
@@ -60,9 +49,8 @@ func (u UserController) Upload(w http.ResponseWriter, r *http.Request) {
 	defer file.Close()
 
 	usersCreated, err := u.userService.Upload(file)
-	if httpCode := assertError(err); httpCode != 0 {
+	if assertError(err, w) {
 		u.log.Error("User.Upload() error:", err)
-		http.Error(w, http.StatusText(httpCode), httpCode)
 		return
 	}
 
